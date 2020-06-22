db0a3ae5685d5eaa7853e0f2fa52ea2e808e8e2e
vpodd <vpodd@10.13.252.204>
2020-06-22 13:01:34 +0530

    first commit......

---
+++ IstGitProject/src/com/git/Test.java
@@ -0,0 +1,18 @@
+package com.git;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		String hungry = "yes";
+		
+		if(hungry == "yes") 
+			
+			System.out.println("eat Somethings");
+		else {
+			System.out.println("Do Work.......");
+		}
+
+	}
+
+}
